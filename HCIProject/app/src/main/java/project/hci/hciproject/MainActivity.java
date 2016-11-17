4a555cd8bdbec2fbcef5ffb1c6a90d12c12388e6
Jake Degioavnni <degiojake@gmail.com>
2016-11-17 17:24:12 +0000

    Testing conditional gyroscope rotations

--- HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
+++ HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
@@ -28,46 +28,61 @@ public class MainActivity extends AppCompatActivity {
         public void onSensorChanged(SensorEvent event) {
             // This timestep's delta rotation to be multiplied by the current rotation
             // after computing it from the gyro sample data.
-//            if (timestamp != 0) {
-//                final float dT = (event.timestamp - timestamp) * NS2S;
-//                // Axis of the rotation sample, not normalized yet.
-//                float axisX = event.values[0];
-//                float axisY = event.values[1];
-//                float axisZ = event.values[2];
-//
-//                // Calculate the angular speed of the sample
-//                float omegaMagnitude = (float) sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-//
-//                // Normalize the rotation vector if it's big enough to get the axis
-//                // (that is, EPSILON should represent your maximum allowable margin of error)
-//                float EPSILON = 0.1f;
-//                if (omegaMagnitude > EPSILON) {
-//                    axisX /= omegaMagnitude;
-//                    axisY /= omegaMagnitude;
-//                    axisZ /= omegaMagnitude;
-//                }
-//
-//                // Integrate around this axis with the angular speed by the timestep
-//                // in order to get a delta rotation from this sample over the timestep
-//                // We will convert this axis-angle representation of the delta rotation
-//                // into a quaternion before turning it into the rotation matrix.
-//                float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-//                float sinThetaOverTwo = (float) sin(thetaOverTwo);
-//                float cosThetaOverTwo = (float) cos(thetaOverTwo);
-//                deltaRotationVector[0] = sinThetaOverTwo * axisX;
-//                deltaRotationVector[1] = sinThetaOverTwo * axisY;
-//                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-//                deltaRotationVector[3] = cosThetaOverTwo;
-//            }
-//            timestamp = event.timestamp;
-//            float[] deltaRotationMatrix = new float[9];
-//            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+            if (timestamp != 0) {
+                final float dT = (event.timestamp - timestamp) * NS2S;
+                // Axis of the rotation sample, not normalized yet.
+                float axisX = event.values[0];
+                float axisY = event.values[1];
+                float axisZ = event.values[2];
+
+                // Calculate the angular speed of the sample
+                float omegaMagnitude = (float) sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                // (that is, EPSILON should represent your maximum allowable margin of error)
+                float EPSILON = 0.1f;
+                if (omegaMagnitude > EPSILON) {
+                    axisX /= omegaMagnitude;
+                    axisY /= omegaMagnitude;
+                    axisZ /= omegaMagnitude;
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+                float sinThetaOverTwo = (float) sin(thetaOverTwo);
+                float cosThetaOverTwo = (float) cos(thetaOverTwo);
+                deltaRotationVector[0] = sinThetaOverTwo * axisX;
+                deltaRotationVector[1] = sinThetaOverTwo * axisY;
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+                deltaRotationVector[3] = cosThetaOverTwo;
+
+                if (deltaRotationVector[0] > 2) {
+                    Log.d("Movement", "UP");
+                } else if (deltaRotationVector[0] < -2) {
+                    Log.d("Movement", "DOWN");
+                } else if (deltaRotationVector[1] > 2) {
+                    Log.d("Movement", "LEFT");
+                } else if (deltaRotationVector[1] < -2) {
+                    Log.d("Movement", "RIGHT");
+                }
+
+
+//                Log.d("MY_APP, x", deltaRotationVector[0]
+//                        + " y: " + deltaRotationVector[1]
+//                        + " z: " + deltaRotationVector[2]);
+            }
+            timestamp = event.timestamp;
+            float[] deltaRotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
             // User code should concatenate the delta rotation we computed with the current rotation
             // in order to get the updated rotation.
 //             rotationCurrent = rotationCurrent * deltaRotationMatrix;
-
-            Log.d("MY_APP, x", String.valueOf(event.values[0]) + " y: " +
-                    String.valueOf(event.values[1]) + " z: " + String.valueOf(event.values[2]));
+//
+//            Log.d("MY_APP, x", String.valueOf(event.values[0]) + " y: " +
+//                    String.valueOf(event.values[1]) + " z: " + String.valueOf(event.values[2]));
         }
 
         @Override
@@ -101,46 +116,4 @@ public class MainActivity extends AppCompatActivity {
             sensorManager.unregisterListener(mLightSensorListener);
         }
     }
-
-//        public void onSensorChanged(SensorEvent event) {
-//        // This timestep's delta rotation to be multiplied by the current rotation
-//        // after computing it from the gyro sample data.
-//        if (timestamp != 0) {
-//            final float dT = (event.timestamp - timestamp) * NS2S;
-//            // Axis of the rotation sample, not normalized yet.
-//            float axisX = event.values[0];
-//            float axisY = event.values[1];
-//            float axisZ = event.values[2];
-//
-//            // Calculate the angular speed of the sample
-//            float omegaMagnitude = (float) sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-//
-//            // Normalize the rotation vector if it's big enough to get the axis
-//            // (that is, EPSILON should represent your maximum allowable margin of error)
-//            float EPSILON = 0.2f;
-//            if (omegaMagnitude > EPSILON) {
-//                axisX /= omegaMagnitude;
-//                axisY /= omegaMagnitude;
-//                axisZ /= omegaMagnitude;
-//            }
-//
-//            // Integrate around this axis with the angular speed by the timestep
-//            // in order to get a delta rotation from this sample over the timestep
-//            // We will convert this axis-angle representation of the delta rotation
-//            // into a quaternion before turning it into the rotation matrix.
-//            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-//            float sinThetaOverTwo = (float) sin(thetaOverTwo);
-//            float cosThetaOverTwo = (float) cos(thetaOverTwo);
-//            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-//            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-//            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-//            deltaRotationVector[3] = cosThetaOverTwo;
-//        }
-//        timestamp = event.timestamp;
-//        float[] deltaRotationMatrix = new float[9];
-//        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-//        // User code should concatenate the delta rotation we computed with the current rotation
-//        // in order to get the updated rotation.
-//        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
-//    }
 }
