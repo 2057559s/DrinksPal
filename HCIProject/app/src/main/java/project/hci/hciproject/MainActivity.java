67b9a6151abfd0ca500ba935c2604583145d9375
jake degiovanni <degiojake@gmail.com>
2016-11-21 16:34:38 +0000

    Moved gyroscope logic to its own class

--- HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
+++ HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
@@ -2,87 +2,19 @@ package project.hci.hciproject;
 
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
-import static java.lang.Math.sqrt;
+import project.hci.hciproject.util.GyroSensorEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager sensorManager;
     private Sensor sensor;
 
-    // Create a constant to convert nanoseconds to seconds.
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4];
-    private float timestamp;
-
-    private SensorEventListener mLightSensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            // This timestep's delta rotation to be multiplied by the current rotation
-            // after computing it from the gyro sample data.
-            if (timestamp != 0) {
-                final float dT = (event.timestamp - timestamp) * NS2S;
-                // Axis of the rotation sample, not normalized yet.
-                float axisX = event.values[0];
-                float axisY = event.values[1];
-                float axisZ = event.values[2];
-
-                // Calculate the angular speed of the sample
-                float omegaMagnitude = (float) sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-
-                // Normalize the rotation vector if it's big enough to get the axis
-                // (that is, EPSILON should represent your maximum allowable margin of error)
-                float EPSILON = 0.1f;
-                if (omegaMagnitude > EPSILON) {
-                    axisX /= omegaMagnitude;
-                    axisY /= omegaMagnitude;
-                    axisZ /= omegaMagnitude;
-                }
-
-                // Integrate around this axis with the angular speed by the timestep
-                // in order to get a delta rotation from this sample over the timestep
-                // We will convert this axis-angle representation of the delta rotation
-                // into a quaternion before turning it into the rotation matrix.
-                float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-                float sinThetaOverTwo = (float) sin(thetaOverTwo);
-                float cosThetaOverTwo = (float) cos(thetaOverTwo);
-                deltaRotationVector[0] = sinThetaOverTwo * axisX;
-                deltaRotationVector[1] = sinThetaOverTwo * axisY;
-                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-                deltaRotationVector[3] = cosThetaOverTwo;
-
-                // TODO see if i can add a time where no sensor data recorded after movement detected
-                if (deltaRotationVector[0] > 0.2) {
-                    Log.d("Movement", "UP");
-                } else if (deltaRotationVector[0] < -0.2) {
-                    Log.d("Movement", "DOWN");
-                } else if (deltaRotationVector[1] > 0.2) {
-                    Log.d("Movement", "RIGHT");
-                } else if (deltaRotationVector[1] < -0.2) {
-                    Log.d("Movement", "LEFT");
-                }
-            }
-            timestamp = event.timestamp;
-            float[] deltaRotationMatrix = new float[9];
-            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-            // User code should concatenate the delta rotation we computed with the current rotation
-            // in order to get the updated rotation.
-//             rotationCurrent = rotationCurrent * deltaRotationMatrix;
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            Log.d("MY_APP", sensor.toString() + " - " + accuracy);
-        }
-    };
+    private SensorEventListener gyroscopeListener = new GyroSensorEventListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         if (sensor != null) {
-            sensorManager.registerListener(mLightSensorListener, sensor,
+            sensorManager.registerListener(gyroscopeListener, sensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
@@ -106,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         if (sensor != null) {
-            sensorManager.unregisterListener(mLightSensorListener);
+            sensorManager.unregisterListener(gyroscopeListener);
         }
     }
 }
