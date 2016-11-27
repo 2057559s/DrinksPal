c4a25b14641e4fb0b52168f13dba7bc120b5f7ca
Jake Degioavnni <degiojake@gmail.com>
2016-11-27 15:28:55 +0000

    Main Activity UI done

--- HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
+++ HCIProject/app/src/main/java/project/hci/hciproject/MainActivity.java
@@ -7,11 +7,19 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import project.hci.hciproject.util.GyroSensorEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainBackground)
+    protected ImageView background;
 
     private SensorManager sensorManager;
     private Sensor sensor;
@@ -22,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Glide.with(this).load(R.drawable.beer).centerCrop().into(background);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
